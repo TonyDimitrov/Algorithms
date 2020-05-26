f5f20903554187f7bd59440cf042d86aa210613e
Toni <01tonydimitrov@gmail.com>
2020-05-26 17:04:31 +0300

    Recursion basics

---
+++ Recursion/Program.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Recursion
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var helper = new RecursionHelper();
+
+            //  Console.WriteLine(helper.Sum(new int[] { 1, 2, 3, 4 }, 0));
+
+            //  Console.WriteLine(helper.Factorial(5));
+
+            //  helper.DrawFigure(5);
+
+            // helper.GenVector(new int[3], 0);
+
+            helper.GenCombinations(new int[] { 1, 2, 3 }, new int[2], 0, 0);
+        }
+    }
+}
---
+++ Recursion/RecursionHelper.cs
@@ -0,0 +1,84 @@
+﻿namespace Recursion
+{
+    using System;
+
+    public class RecursionHelper
+    {
+        public int Sum(int[] numbers, int index)
+        {
+            if (numbers.Length == index)
+            {
+                return 0;
+            }
+
+            return Sum(numbers, index + 1) + numbers[index];
+        }
+
+        public long Factorial(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+
+            return Factorial(number - 1) * number;
+        }
+
+        /*
+         Samply by given value 2
+            **
+            *
+            #
+            ##
+         */
+        public void DrawFigure(int n)
+        {
+            if (n == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine(new string('*', n));
+
+            DrawFigure(n - 1);
+
+            Console.WriteLine(new string('#', n));
+        }
+
+        public void GenVector(int[] vector, int index)
+        {
+            if (vector.Length <= index)
+            {
+                Console.WriteLine(string.Join(" ", vector));
+
+                return;
+            }
+            else
+            {
+                for (int i = 0; i <= 1; i++)
+                {
+                    vector[index] = i;
+
+                    GenVector(vector, index + 1);
+                }
+            }
+        }
+
+        public void GenCombinations(int[] set, int[] vector, int index, int offset)
+        {
+            if (index == vector.Length)
+            {
+                Console.WriteLine(string.Join(" ", vector));
+            }
+            else
+            {
+                for (int i = offset; i < set.Length; i++)
+                {
+                    vector[index] = set[i];
+
+                    GenCombinations(set, vector, index + 1, i + 1);
+                }
+            }
+        }
+    }
+}
